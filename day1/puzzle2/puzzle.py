7810bcaf82eca4573370ba7e584cbaa95661627d
Roberto Filipe Manso Barreto <a21123@alunos.ipca.pt>
2021-12-01 16:41:56 +0000

    Code clean up~

--- day1/puzzle2/puzzle.py
+++ day1/puzzle2/puzzle.py
@@ -5,10 +5,10 @@ mesurements = contents.split('\n')
 counter = 0
 lastSum = 0
 
-for i in range(0, len(mesurements)):
+for i in range(1, len(mesurements)):
     if i+1 < len(mesurements) and i+2 < len(mesurements):
         currSum = int(mesurements[i]) + int(mesurements[i+1]) + int(mesurements[i+2])
-        if currSum > lastSum and i > 0:
+        if currSum > lastSum:
             counter += 1
         lastSum = currSum
 
